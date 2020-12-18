2039baf1882769c29853d0807ae701d811860eae
Jacek <jacek@g.pl>
2020-12-18 20:27:43 +0100

    dodanie layout

--- app/src/main/java/jajaceek/weatherteller/MainActivity.java
+++ app/src/main/java/jajaceek/weatherteller/MainActivity.java
@@ -3,12 +3,53 @@ package jajaceek.weatherteller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // przypisanie wartości do poszczególnych przycisków
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+        // przypisanie listners do poszczególnych przycisków
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Nacisnąłeś City", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "nacisnąłeś ID", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Nacisnąłeś Name " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
